3a6e8bec480ab05142a4760f01db8c87de93bcd5
6rout <theisenm@msn.com>
2019-12-02 14:32:45 -0700

    Added early draft of board/piece objects

---
+++ app/src/main/java/com/example/gomoku/Piece.java
@@ -0,0 +1,22 @@
+package com.example.gomoku;
+
+public class Piece {
+    // Enum this in -1 empty, 0 black, 1 white.. defaults to empty
+    private int color = -1;
+    // x,y grid value
+    private int[] coords;
+
+    // Public constructor to initialize board with blanks
+    public Piece (int x, int y) {
+        this.coords = new int[]{x,y};
+    }
+
+    // Public constructor to initialize a black/white piece
+    public Piece (int x, int y, int color) {
+        this.coords = new int[]{x,y};
+        assert (color == 0 || color == 1);
+        this.color = color;
+    }
+
+    public int color() {return this.color;}
+}
