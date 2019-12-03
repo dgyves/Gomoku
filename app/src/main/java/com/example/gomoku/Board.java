39b52c5529f5cc29ec099f8b6224e18cd0fc7fe4
6rout <theisenm@msn.com>
2019-12-02 18:30:13 -0700

    Added a bunch of logic to board.java for checking if win condition is met

--- app/src/main/java/com/example/gomoku/Board.java
+++ app/src/main/java/com/example/gomoku/Board.java
@@ -3,12 +3,15 @@ package com.example.gomoku;
 public class Board {
     private Piece[][] grid;
     // Track AI (0) or human (1)
-    private int p1;
+    private final int p1 = 1;
     private int p2;
     private int turn;
 
-    // Constructor - initialize the board with blank Pieces
-    public Board (int p1, int p2) {
+    /**
+     * Constructor - initialize the board with blank Pieces
+     * @param p2 sets player 2 as human or AI
+     */
+    public Board (int p2) {
         // Generate game board
         this.grid = new Piece[15][15];
         for (int y = 0; y < 15; y++) {
@@ -17,14 +20,13 @@ public class Board {
             }
         }
 
-        // Set players
-        this.p1 = p1;
+        // Set whether player 2 is AI (0) or human (1)
         this.p2 = p2;
         turn = 1;
     }
 
     // Return 1 for p1, 2 for p2
-    public int whoseTurn() {
+    public int whoseTurnIsItAnyway() {
         return this.turn;
     }
 
@@ -34,32 +36,159 @@ public class Board {
         else {this.turn = 1;}
     }
 
-    // Called each time a Piece has just been placed.
-    // Check neighboring pieces on all 3 axes to determine if game has been won
-    public boolean gameWon(int player, int x, int y) {
+    /**
+     * Called each time a Piece has just been placed.
+     * Check neighboring pieces on all 4 axes to determine if game has been won
+     * @param thisPlayer represents player that made this move
+     * @param x x-coord of piece that was just played
+     * @param y y-coord of piece that was just played
+     **/
+    public boolean gameWon(int thisPlayer, int x, int y) {
         // Horizontal
+        int WE_Count = checkWtoE(thisPlayer,1-thisPlayer,true,true,x,y);
+        if (WE_Count >= 5) {return true;}
 
         // Vertical
+        int NS_Count = checkNtoS(thisPlayer,1-thisPlayer,true,true, x,y);
+        if (NS_Count >= 5) {return true;}
 
-        // Diagonal
-        // TODO: Real return value
+        // Diagonals
+        int NWSE_Count = checkNWtoSE(thisPlayer,1-thisPlayer,true,true,x,y);
+        if (NWSE_Count >= 5) {return true;}
+        int SWNE_Count = checkSWtoNE(thisPlayer,1-thisPlayer,true,true,x,y);
+        if (SWNE_Count >= 5) {return true;}
+
     return false;
     }
 
-    private int checkLR (int player, int otherPlayer, int x, int y) {
-        // Out of grid bounds
+    /**
+     * Check the West-to-East (horizontal) axis to see how many of <thisColor> there are in a row
+     @param thisColor represents player that just made the move
+     @param otherColor represents opponent
+     @param checkingWest are we currently checking west?  or east?  West is checked first
+     @param firstCall this is a recursive function.  is this the first (original) call?
+     @param x x-coord of the piece that was just played (on first call)
+     @param y y-coord of the piece that was just played (on first call)
+    **/
+    private int checkWtoE (int thisColor, int otherColor, boolean checkingWest, boolean firstCall, int x, int y) {
+        // Out of grid bounds (edge of game board passed)
         if (x < 0 || y < 0 || x > 14 || y > 14) {
             return 0;
         }
 
         // Blank space or opponent piece encountered
-        if (grid[x][y].color() == -1 || grid[x][y].color() == otherPlayer) {
+        if (grid[x][y].color() != thisColor) {
             return 0;
         }
 
         // Recurse
-        // TODO: Verify if this results in too many checks on each piece, I think it may check
-        // TODO: In both directions for EACH piece encountered
-        return 1 + checkLR(player,otherPlayer,x-1,y) + checkLR(player,otherPlayer,x+1,y);
+        if (firstCall) {
+            return 1 + checkWtoE(thisColor, otherColor, true, false, x - 1, y) +
+                    checkWtoE(thisColor, otherColor, false,false, x + 1, y);
+        }
+        else if (checkingWest) {
+            return 1 + checkWtoE(thisColor, otherColor, true, false, x-1,y);
+        }
+        else { //checking East
+            return 1 + checkWtoE(thisColor, otherColor, false, false, x + 1, y);
+        }
+    }
+    /**
+     * Check the North-to-South (vertical) axis to see how many of <thisColor> there are in a row
+     @param thisColor represents player that just made the move
+     @param otherColor represents opponent
+     @param checkingN are we currently checking north? or south?  North is checked first
+     @param firstCall this is a recursive function.  is this the first (original) call?
+     @param x x-coord of the piece that was just played (on first call)
+     @param y y-coord of the piece that was just played (on first call)
+     **/
+    private int checkNtoS (int thisColor, int otherColor, boolean checkingN, boolean firstCall, int x, int y) {
+        // Out of grid bounds (edge of game board passed)
+        if (x < 0 || y < 0 || x > 14 || y > 14) {
+            return 0;
+        }
+
+        // Blank space or opponent piece encountered
+        if (grid[x][y].color() != thisColor) {
+            return 0;
+        }
+
+        // Recurse
+        if (firstCall) {
+            return 1 + checkNtoS(thisColor, otherColor, true, false, x, y-1) +
+                    checkNtoS(thisColor, otherColor, false,false, x, y+1);
+        }
+        else if (checkingN) {
+            return 1 + checkNtoS(thisColor, otherColor, true, false, x,y-1);
+        }
+        else { //checking South
+            return 1 + checkNtoS(thisColor, otherColor, false, false, x, y+1);
+        }
+    }
+
+    /**
+     * Check the NW to SE (diagonal) axis to see how many of <thisColor> there are in a row
+     @param thisColor represents player that just made the move
+     @param otherColor represents opponent
+     @param checkingNW are we currently checking northwest?  or southeast?  Northwest is checked first
+     @param firstCall this is a recursive function.  is this the first (original) call?
+     @param x x-coord of the piece that was just played (on first call)
+     @param y y-coord of the piece that was just played (on first call)
+     **/
+    private int checkNWtoSE (int thisColor, int otherColor, boolean checkingNW, boolean firstCall, int x, int y) {
+        // Out of grid bounds (edge of game board passed)
+        if (x < 0 || y < 0 || x > 14 || y > 14) {
+            return 0;
+        }
+
+        // Blank space or opponent piece encountered
+        if (grid[x][y].color() != thisColor) {
+            return 0;
+        }
+
+        // Recurse
+        if (firstCall) {
+            return 1 + checkNWtoSE(thisColor, otherColor, true, false, x-1, y-1) +
+                    checkNWtoSE(thisColor, otherColor, false,false, x+1, y+1);
+        }
+        else if (checkingNW) {
+            return 1 + checkNWtoSE(thisColor, otherColor, true, false, x-1,y-1);
+        }
+        else { //checking Southeast
+            return 1 + checkNWtoSE(thisColor, otherColor, false, false, x+1, y+1);
+        }
+    }
+
+    /**
+     * Check the SW to NE (diagonal) axis to see how many of <thisColor> there are in a row
+     @param thisColor represents player that just made the move
+     @param otherColor represents opponent
+     @param checkingSW are we currently checking southwest?  or northeast?  Southwest is checked first
+     @param firstCall this is a recursive function.  is this the first (original) call?
+     @param x x-coord of the piece that was just played (on first call)
+     @param y y-coord of the piece that was just played (on first call)
+     **/
+    private int checkSWtoNE (int thisColor, int otherColor, boolean checkingSW, boolean firstCall, int x, int y) {
+        // Out of grid bounds (edge of game board passed)
+        if (x < 0 || y < 0 || x > 14 || y > 14) {
+            return 0;
+        }
+
+        // Blank space or opponent piece encountered
+        if (grid[x][y].color() != thisColor) {
+            return 0;
+        }
+
+        // Recurse
+        if (firstCall) {
+            return 1 + checkSWtoNE(thisColor, otherColor, true, false, x-1, y+1) +
+                    checkSWtoNE(thisColor, otherColor, false,false, x+1, y-1);
+        }
+        else if (checkingSW) {
+            return 1 + checkSWtoNE(thisColor, otherColor, true, false, x-1,y+1);
+        }
+        else { //checking Northeast
+            return 1 + checkSWtoNE(thisColor, otherColor, false, false, x+1, y-1);
+        }
     }
 }
